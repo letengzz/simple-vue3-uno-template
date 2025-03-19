fa9d366ee9b7704c7219cebcf7ca3338beaf72c1
letengzz <2020885569@qq.com>
2025-03-20 01:14:18 +0800

    chore: add unocss reset config

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,9 @@ import App from './App.vue'
 import { useRouter } from './router'
 import { usePinia } from './stores'
 import 'virtual:uno.css' // 引入 uno.css
-import '@unocss/reset/tailwind.css'
+// import '@unocss/reset/tailwind.css'
+import '@unocss/reset/sanitize/sanitize.css'
+import '@unocss/reset/sanitize/assets.css'
 // 引入全局样式
 import '@/styles/index.scss'
 
